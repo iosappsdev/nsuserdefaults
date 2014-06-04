5053f146f973e149d9637868cefeb7d66bcd9314
iOS Apps Development <info@iosappsdev.org>
2014-06-03 23:01:28 -0700

    Initial Commit
    Using NSUserDefaults, Saving and Loading Data.

--- nsuserdefaults/ViewController.m
+++ nsuserdefaults/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *tf_name;
+@property (weak, nonatomic) IBOutlet UITextField *tf_email;
+@property (weak, nonatomic) IBOutlet UITextField *tf_age;
+- (IBAction)saveData:(id)sender;
 
 @end
 
@@ -18,12 +22,39 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *name = [defaults objectForKey:@"firstName"];
+    NSString *email = [defaults objectForKey:@"email"];
+    int age = [defaults integerForKey:@"age"];
+    NSString *ageString = [NSString stringWithFormat:@"%i", age];
+
+    _tf_name.text = name;
+    _tf_email.text = email;
+    _tf_age.text = ageString;
+
+    
+    _tf_age.keyboardType = UIKeyboardTypeNumberPad;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)saveData:(id)sender {
+    
+    [_tf_age resignFirstResponder];
+    [_tf_name resignFirstResponder];
+    [_tf_email resignFirstResponder];
+    
+    NSString *name = _tf_name.text;
+    NSString *email = _tf_email.text;
+    int age = [[_tf_age text] integerValue];
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    [defaults setObject:name forKey:@"firstName"];
+    [defaults setObject:email forKey:@"email"];
+    [defaults setInteger:age forKey:@"age"];
+    
+    [defaults synchronize];
+    
 }
+
 
 @end
